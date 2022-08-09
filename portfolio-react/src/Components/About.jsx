47ec059cdfe48c44b41c85f4de36bdd6aaa364a0
riley-laptop <rileys1000@gmail.com>
2022-08-10 10:55:16 +1200

    Changed some css + about page

--- portfolio-react/src/Components/About.jsx
+++ portfolio-react/src/Components/About.jsx
@@ -1,5 +1,20 @@
+import Jason from './Images/jason.jpg'
+import Button from './Button';
+
 export default function About() {
     return (
-        <div className="s">About</div>
+        <div className="center50vw">
+            <div className="aboutMegaFlexContainer">
+                <div className="aboutFlex1 margin1rem"><h4>JASON SMITH</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint  occaecat cupidatat non proident, sunt in culpa qui officia deserunt  mollit anim id est laborum."</div>
+                <div className="aboutFlex2 margin1rem">
+                    <div className="aboutInnerFlexLeft margin1rem"><h4>TITLE</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ext dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</div>
+                    <div className="aboutInnerFlexRight">
+                        <div className="aboutInnerFlexImage"><img src={Jason} alt="Jason Smith" srcset={Jason} /></div>
+                        <div className="aboutInnerFlexButton"><Button className={"PButt"} onClick="" id="toServices">Book A Meeting</Button></div>
+                    </div>
+                </div>
+                <div className="aboutFlex3 margin1rem"><h4>TITLE 2</h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ext dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</div>
+            </div>
+        </div>
     );
 }
--- portfolio-react/src/Components/Home.jsx
+++ portfolio-react/src/Components/Home.jsx
@@ -2,14 +2,15 @@ import Button from './Button';
 
 export default function Home(props) {
     return (
-        <div id="center50vw">
+        <div class="center50vw">
             <div id="homeTextGrid">
                 <div id="homeText1">
-
+                <h4>WHAT IS FIVE PEAKS ACCOUNTING?</h4>
                 Five Peaks Accounting Ltd is a small Queenstown Accountant which is a Chartered Accounting firm that takes the hassle out of business accounting and advice.
 <br /><br />
                 Whether it's setting up an accounting system to match your business requirements or professional advice about taxation, cashflow, business plan, succession planning or starting a business, we're here to help and meet at our Frankton Queenstown office.
 <br /><br />
+                <h4>PRICING.</h4>
                 Our pricing is transparent-whether it's a one-off project like a business plan or forecast cashflow-we'll let you know the price before we start. Many of our clients like the monthly pricing plans we offer as these spread the cost throughout the year. And there's no extra charge when you want to call us on the phone with a quick query.
 <br /><br />
                 We pride ourselves on being an accessable Queenstown Accountant. Our practice has been operating since 2015 and we offer a wide range of services to suit all budgets, ensuring that you get the best care and support that you deserve.
